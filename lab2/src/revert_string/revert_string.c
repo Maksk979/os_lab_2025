0b2278a7cee45cf9cfcaa6e96315d2352d12d869
Maksk979 <maksk9753@gmail.com>
2025-09-19 18:28:29 +0000

    Добавлены исходные файлы из папки revert_string

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,15 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	int length = strlen(str);
+	
+	for(int i = 0; i < length / 2; i++){
+		
+		char tmp = str[i];
+		str[i] = str[length - i - 1];
+		str[length - i - 1] = tmp;
+	}
 }
 
